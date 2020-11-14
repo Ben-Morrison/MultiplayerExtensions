62fc16667b58da4616930611da0a8d5d164bc4e2
Zingabopp <Zingabopp@gmail.com>
2020-11-14 13:44:54 -0600

    Voice chat experiments

---
+++ MultiplayerExtensions/VOIP/VoipPacket.cs
@@ -0,0 +1,56 @@
+﻿using LiteNetLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerExtensions.VOIP
+{
+    public class VoipPacket : INetSerializable, IPoolablePacket
+    {
+        public string? playerId;
+        public byte[]? data;
+        public int index;
+
+        protected static PacketPool<VoipPacket> pool
+        {
+            get
+            {
+                return ThreadStaticPacketPool<VoipPacket>.pool;
+            }
+        }
+
+        public static VoipPacket Create(string playerId, int index, byte[] data)
+        {
+            VoipPacket packet = pool.Obtain();
+            packet.playerId = playerId;
+            packet.index = index;
+            packet.data = data;
+            return packet;
+        }
+
+        public void Deserialize(NetDataReader reader)
+        {
+            playerId = reader.GetString();
+            index = reader.GetInt();
+            data = reader.GetBytesWithLength();
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put(playerId);
+            writer.Put(index);
+            if (data != null)
+                writer.PutBytesWithLength(data, 0, data.Length);
+            else
+                Plugin.Log?.Warn($"Trying to serialize a 'VoipPacket' with null data.");
+        }
+
+        public void Release()
+        {
+            data = null;
+            pool.Release(this);
+        }
+    }
+}
---
+++ MultiplayerExtensionsTests/AudioUtils_Tests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiplayerExtensions.VOIP;
+
+namespace MultiplayerExtensionsTests
+{
+    [TestClass]
+    public class AudioUtils_Tests
+    {
+        [TestMethod]
+        public void FloatAryToBytes()
+        {
+            float[] original = new float[] { .434f, .23432f, .2342f, .9349f };
+            byte[] byteAry = new byte[original.Length * 2];
+
+            AudioUtils.Convert(original, ref byteAry);
+
+            float[] reverted = new float[original.Length];
+            AudioUtils.Convert(byteAry, ref reverted);
+
+            for(int i = 0; i < original.Length; i++)
+            {
+                Console.WriteLine($"{original[i]} -> {byteAry[i].ToString("X2")}{byteAry[i+1].ToString("X2")} -> {reverted[i]}");
+            }
+            for (int i = 0; i < original.Length; i++)
+            {
+                Assert.AreEqual(Math.Round(original[i], 3), Math.Round(reverted[i], 3), $"Mismatch at '{i}', expected '{original[i]}' but got '{reverted[i]}'");
+            }
+        }
+        [TestMethod]
+        public void ByteAryToFloats()
+        {
+            byte[] original = new byte[] { 0x37, 0x8C, 0x37, 0x8C };
+            float[] floatAry = new float[original.Length / 2];
+            AudioUtils.Convert(original, ref floatAry);
+
+            byte[] reverted = new byte[original.Length];
+            AudioUtils.Convert(floatAry, ref reverted);
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                Assert.AreEqual(original[i], reverted[i]);
+            }
+        }
+    }
+}
